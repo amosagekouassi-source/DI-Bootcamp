e5b33ef13a736d03a8223fc3c54ad7e631c34963
amosagekouassi-source <amosagekouassi@gmail.com>
2026-05-20 10:40:49 +0000

    Ajout de l'exercice sur la marque du PC

---
+++ Semaine_01/Jour_01/Exercice_2026_19/exercice_gold.py
@@ -0,0 +1,26 @@
+#Exercise 1: What is the Season?
+
+# On met un texte vide au début (index 0) pour que Janvier devienne le numéro 1 !
+
+noms_mois = ["", "January", "February", "March", "April", "May", "June", 
+             "July", "August", "September", "October", "November", "December"]
+
+# Demander le numéro du mois à l'utilisateur
+
+numero = int(input("Entrez le numéro du mois (1-12) : "))
+
+# Récupérer le nom correspondant dans la liste
+
+mois_choisi = noms_mois[numero]
+
+Saison = ""
+
+if numero in range(3, 5):
+    saison = "Spring"
+elif numero in range(6, 8):
+    saison = "Summer"
+elif numero in range(9, 11):
+    saison = "Autumn"
+else:
+    saison = "Winter"
+print(f"Le mois que vous avez choisi est {mois_choisi} et la saison correspondante est {saison}.")
