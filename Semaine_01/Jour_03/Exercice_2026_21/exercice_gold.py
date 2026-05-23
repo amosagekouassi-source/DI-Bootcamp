be9946e5d26d4cba11f362d2162c5e36966ceab3
amosagekouassi-source <amosagekouassi@gmail.com>
2026-05-23 21:13:42 +0000

    Exercice_xp 3 du jour 4

--- Semaine_01/Jour_03/Exercice_2026_21/exercice_gold.py
+++ Semaine_01/Jour_03/Exercice_2026_21/exercice_gold.py
@@ -1,3 +1,5 @@
+#Exercise 1 : Geometry
+
 class circle():
 
   def __init__(self, radius=1.0):
@@ -13,3 +15,23 @@ class circle():
 my_circle = circle(5.0) # Example with radius 5.0
 
 print(f"Un cercle est un figure géométrique dont le périmètre est p= {my_circle.perimeter()} et dont l'aire = {my_circle.area()}.")
+
+#Exercise 2 : Custom List Class
+
+class MyList():
+    def __init__(self, letters):
+        self.letters = letters
+        self.length = len(letters)
+
+    def reverse_list(self):
+        return list(reversed(self.letters))
+
+    def sort_list(self):
+        return sorted(self.letters)
+
+    
+list1 = MyList(["f", "t", "q", "h", "x", "c", "m"])
+print(list1.reverse_list())
+print(list1.sort_list())
+
+#
